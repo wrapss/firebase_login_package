6c144717a9cddde5ec059c9eeedfbe64d61bf29e
sakayase <simon.ponitzki@gmail.com>
2021-12-09 17:31:42 +0100

    mail & password login

--- lib/src/firebase_login_package_base.dart
+++ lib/src/firebase_login_package_base.dart
@@ -49,6 +49,23 @@ class AuthenticationRepository {
     }
   }
 
+  Future<void> loginWithEmailAndPassword(String email, String password) async {
+    try {
+      UserCredential userCredential =
+          await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      user = userCredential.user;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
+      }
+    }
+  }
+
   Future<void> logOut() async {
     try {
       await Future.wait([
