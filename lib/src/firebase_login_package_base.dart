e1570ac7f98aa82dff465d57eef17ac55a26273f
sakayase <simon.ponitzki@gmail.com>
2022-01-31 13:00:38 +0100

    modif errors loginWithEmailAndPassword

--- lib/src/firebase_login_package_base.dart
+++ lib/src/firebase_login_package_base.dart
@@ -57,12 +57,8 @@ class AuthenticationRepository {
         password: password,
       );
       user = userCredential.user;
-    } on FirebaseAuthException catch (e) {
-      if (e.code == 'user-not-found') {
-        print('No user found for that email.');
-      } else if (e.code == 'wrong-password') {
-        print('Wrong password provided for that user.');
-      }
+    } on FirebaseAuthException {
+      rethrow;
     }
   }
 
