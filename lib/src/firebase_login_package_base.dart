fbccb7e2ae759141062432cc55765c048d0de357
alpha SOW <alphaseul@yahoo.fr>
2022-03-02 08:53:20 +0100

    update interface

--- lib/src/firebase_login_package_base.dart
+++ lib/src/firebase_login_package_base.dart
@@ -9,95 +9,71 @@ class AuthenticationRepository {
   late String _verificationId;
 
   Future<void> verifyPhoneNumber(String phoneNumber) async {
-    try {
-      await firebaseAuth.verifyPhoneNumber(
-        phoneNumber: phoneNumber,
-        timeout: const Duration(seconds: 60),
-        verificationCompleted: (PhoneAuthCredential phoneAuthCredential) async {
-          await firebaseAuth.signInWithCredential(phoneAuthCredential);
-        },
-        verificationFailed: (FirebaseAuthException authException) {
-          message = authException.message;
-        },
-        codeSent: (String verificationId, [int? forceResendingToken]) {
-          _verificationId = verificationId;
-        },
-        codeAutoRetrievalTimeout: (String verificationId) {
-          _verificationId = verificationId;
-        },
-      );
-    } on FirebaseAuthException catch (e) {
-      return print(e);
-    }
+    return await firebaseAuth.verifyPhoneNumber(
+      phoneNumber: phoneNumber,
+      timeout: const Duration(seconds: 60),
+      verificationCompleted: (PhoneAuthCredential phoneAuthCredential) async {
+        await firebaseAuth.signInWithCredential(phoneAuthCredential);
+      },
+      verificationFailed: (FirebaseAuthException authException) {
+        message = authException.message;
+      },
+      codeSent: (String verificationId, [int? forceResendingToken]) {
+        _verificationId = verificationId;
+      },
+      codeAutoRetrievalTimeout: (String verificationId) {
+        _verificationId = verificationId;
+      },
+    );
   }
 
   Future<void> signInWithPhoneNumber(String smsCode) async {
-    try {
-      final PhoneAuthCredential credential = PhoneAuthProvider.credential(
-        verificationId: _verificationId,
-        smsCode: smsCode,
-      );
-      user = (await firebaseAuth.signInWithCredential(credential)).user!;
-    } on FirebaseAuthException catch (e) {
-      return print(e);
-    }
+    final PhoneAuthCredential credential = PhoneAuthProvider.credential(
+      verificationId: _verificationId,
+      smsCode: smsCode,
+    );
+    user = (await firebaseAuth.signInWithCredential(credential)).user!;
   }
 
   Future<void> loginWithEmailAndPassword(String email, String password) async {
-    try {
-      UserCredential userCredential =
-          await FirebaseAuth.instance.signInWithEmailAndPassword(
-        email: email,
-        password: password,
-      );
-      user = userCredential.user;
-    } on FirebaseAuthException catch (e) {
-      return print(e);
-    }
+    UserCredential userCredential =
+        await FirebaseAuth.instance.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+    user = userCredential.user;
   }
 
   Future<void> verifyPhoneNumberUpdate(String phoneNumber) async {
-    try {
-      await firebaseAuth.verifyPhoneNumber(
-        phoneNumber: phoneNumber,
-        timeout: const Duration(seconds: 60),
-        verificationCompleted: (PhoneAuthCredential phoneAuthCredential) async {
-          await user!.updatePhoneNumber(phoneAuthCredential);
-        },
-        verificationFailed: (FirebaseAuthException authException) {
-          message = authException.message;
-        },
-        codeSent: (String verificationId, [int? forceResendingToken]) {
-          _verificationId = verificationId;
-        },
-        codeAutoRetrievalTimeout: (String verificationId) {
-          _verificationId = verificationId;
-        },
-      );
-    } on FirebaseAuthException catch (e) {
-      return print(e);
-    }
+    return await firebaseAuth.verifyPhoneNumber(
+      phoneNumber: phoneNumber,
+      timeout: const Duration(seconds: 60),
+      verificationCompleted: (PhoneAuthCredential phoneAuthCredential) async {
+        await user!.updatePhoneNumber(phoneAuthCredential);
+      },
+      verificationFailed: (FirebaseAuthException authException) {
+        message = authException.message;
+      },
+      codeSent: (String verificationId, [int? forceResendingToken]) {
+        _verificationId = verificationId;
+      },
+      codeAutoRetrievalTimeout: (String verificationId) {
+        _verificationId = verificationId;
+      },
+    );
   }
 
   Future<void> updatePhoneNumber(String smsCode) async {
-    try {
-      final PhoneAuthCredential credential = PhoneAuthProvider.credential(
-        verificationId: _verificationId,
-        smsCode: smsCode,
-      );
-      await user!.updatePhoneNumber(credential);
-    } on FirebaseAuthException catch (e) {
-      return print(e);
-    }
+    final PhoneAuthCredential credential = PhoneAuthProvider.credential(
+      verificationId: _verificationId,
+      smsCode: smsCode,
+    );
+    return await user!.updatePhoneNumber(credential);
   }
 
   Future<void> logOut() async {
-    try {
-      await Future.wait([
-        firebaseAuth.signOut(),
-      ]);
-    } on FirebaseAuthException catch (e) {
-      return print(e);
-    }
+    await Future.wait([
+      firebaseAuth.signOut(),
+    ]);
   }
 }
