4fc82292babfe0c5df88266bf2c4ebcd2d7138ba
alpha SOW <alphaseul@yahoo.fr>
2022-03-13 20:21:24 +0100

    update package clean code

--- lib/src/firebase_login_package_base.dart
+++ lib/src/firebase_login_package_base.dart
@@ -2,80 +2,90 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthenticationRepository {
-  AuthenticationRepository({FirebaseAuth? firebaseAuth})
-      : _firebaseAuth = firebaseAuth ?? FirebaseAuth.instance;
-  final FirebaseAuth _firebaseAuth;
+  AuthenticationRepository(this.firebaseAuth);
 
-  late String _verificationId;
+  final FirebaseAuth firebaseAuth;
+  User? user;
+  String? message;
+  late String verificationId;
 
   Future<void> verifyPhoneNumber(String phoneNumber) async {
-    await _firebaseAuth.verifyPhoneNumber(
+    return await firebaseAuth.verifyPhoneNumber(
       phoneNumber: phoneNumber,
       timeout: const Duration(seconds: 60),
       verificationCompleted: (PhoneAuthCredential phoneAuthCredential) async {
-        await _firebaseAuth.signInWithCredential(phoneAuthCredential);
+        await firebaseAuth.signInWithCredential(phoneAuthCredential);
       },
-      verificationFailed: (FirebaseAuthException authException) {},
+      verificationFailed: (FirebaseAuthException authException) {
+        message = authException.message;
+      },
       codeSent: (String verificationId, [int? forceResendingToken]) {
-        _verificationId = verificationId;
+        this.verificationId = verificationId;
       },
       codeAutoRetrievalTimeout: (String verificationId) {
-        _verificationId = verificationId;
+        this.verificationId = verificationId;
       },
     );
   }
 
   Future<void> signInWithPhoneNumber(String smsCode) async {
     final PhoneAuthCredential credential = PhoneAuthProvider.credential(
-      verificationId: _verificationId,
+      verificationId: verificationId,
       smsCode: smsCode,
     );
-    _firebaseAuth.signInWithCredential(credential);
+    user = (await firebaseAuth.signInWithCredential(credential)).user!;
   }
 
   Future<void> loginWithEmailAndPassword(String email, String password) async {
     UserCredential userCredential =
-        await FirebaseAuth.instance.signInWithEmailAndPassword(
+    await FirebaseAuth.instance.signInWithEmailAndPassword(
       email: email,
       password: password,
     );
+    user = userCredential.user;
   }
 
   Future<void> verifyPhoneNumberUpdate(String phoneNumber) async {
-    return await _firebaseAuth.verifyPhoneNumber(
+    return await firebaseAuth.verifyPhoneNumber(
       phoneNumber: phoneNumber,
       timeout: const Duration(seconds: 60),
-      verificationCompleted: (PhoneAuthCredential phoneAuthCredential) async {},
-      verificationFailed: (FirebaseAuthException authException) {},
+      verificationCompleted: (PhoneAuthCredential phoneAuthCredential) async {
+        await user!.updatePhoneNumber(phoneAuthCredential);
+      },
+      verificationFailed: (FirebaseAuthException authException) {
+        message = authException.message;
+      },
       codeSent: (String verificationId, [int? forceResendingToken]) {
-        _verificationId = verificationId;
+        this.verificationId = verificationId;
       },
       codeAutoRetrievalTimeout: (String verificationId) {
-        _verificationId = verificationId;
+        this.verificationId = verificationId;
       },
     );
   }
 
   Future<void> updatePhoneNumber(String smsCode) async {
     final PhoneAuthCredential credential = PhoneAuthProvider.credential(
-      verificationId: _verificationId,
+      verificationId: verificationId,
       smsCode: smsCode,
     );
+    return await user!.updatePhoneNumber(credential);
   }
 
   Future<void> singInWithGoogle() async {
     final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
     final GoogleSignInAuthentication? googleAuth =
-        await googleUser?.authentication;
+    await googleUser?.authentication;
     final credential = GoogleAuthProvider.credential(
       accessToken: googleAuth?.accessToken,
       idToken: googleAuth?.idToken,
     );
+    user = (await firebaseAuth.signInWithCredential(credential)).user!;
   }
 
   Future<void> logOut() async {
     await Future.wait([
-      _firebaseAuth.signOut(),
+      firebaseAuth.signOut(),
     ]);
   }
 }
